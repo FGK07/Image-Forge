20cdfadeb39c8d5c0768610b18846edb1bf897a3
FGK07 <eghakuncorozxzxz@gmail.com>
2026-07-21 08:55:24 +0700

    feat(app): initialize application

---
+++ js/app.js
@@ -0,0 +1,28 @@
+/* ==========================================================
+    ImageForge
+    app.js
+========================================================== */
+
+document.addEventListener(
+
+    "DOMContentLoaded",
+
+    () => {
+
+        console.log(
+
+            "%cImageForge Loaded",
+
+            "color:#3b82f6;font-size:16px;font-weight:bold"
+
+        );
+
+        console.log(
+
+            "Version 1.0.0"
+
+        );
+
+    }
+
+);
